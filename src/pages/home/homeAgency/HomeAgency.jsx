b7f03fdf0bc6e01af9e49f5b2319997d78be872a
rohitsingh7879 <rohitsingh998195@gmail.com>
2025-05-09 09:49:13 +0530

    Added: code for slider

--- src/pages/home/homeAgency/HomeAgency.jsx
+++ src/pages/home/homeAgency/HomeAgency.jsx
@@ -1,4 +1,3 @@
-
 import {
   partner1,
   partner2,
@@ -9,6 +8,9 @@ import {
   partner7,
 } from "@/assets/images";
 
+import React from "react";
+import Slider from "react-slick";
+import "../../../assets/css/customSlick.css";
 const partners = [
   partner1,
   partner2,
@@ -19,31 +21,57 @@ const partners = [
   partner7,
 ];
 
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 1000,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  variableWidth: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        variableWidth: false,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        variableWidth: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        variableWidth: false,
+      },
+    },
+  ],
+};
+
 export default function homeAgency() {
   return (
     <section className="agency_partner">
-      <div className="container">
-        <div className="text-center title_area">
-          <h2>Agency Partners</h2>
-        </div>
-
-        {/* <Carousel opts={{ align: "start", loop: true }} className="agency_logo">
-          <CarouselContent>
-            {partners.map((logo, index) => (
-              <CarouselItem
-                key={index}
-                className="basis-1/2 md:basis-1/2 lg:basis-1/3 px-2"
-              >
-                <div className="flex justify-center items-center">
-                  <img src={logo} alt={`Partner ${index + 1}`} />
-                </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          <CarouselPrevious />
-          <CarouselNext />
-        </Carousel> */}
+    <div className="container">
+      <div className="text-center title_area">
+        <h2>Agency Partners</h2>
       </div>
-    </section>
+      <div className="agency_logo slider slik_slider">
+        <Slider {...settings}>
+          {partners?.map((src, index) => (
+            <div key={index}>
+              <img src={src} alt={`Partner ${index + 1}`} />
+            </div>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  </section>
   );
 }
---
+++ src/pages/property/categoryWiseList/CategoryWiseList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CategoryWiseList() {
+  return (
+    <div>CategoryWiseList</div>
+  )
+}
+
+export default CategoryWiseList

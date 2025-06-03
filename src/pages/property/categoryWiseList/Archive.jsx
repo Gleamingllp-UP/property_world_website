892ce3d08ff242d4ec8f14a283f8c3fb3988b1c9
Gleaming45 <jahid@gleamingllp.com>
2025-06-03 11:42:43 +0530

    add new code to category page

--- src/pages/property/categoryWiseList/Archive.jsx
+++ src/pages/property/categoryWiseList/Archive.jsx
@@ -5,10 +5,12 @@ import ArchiveLocation from "./ArchiveLocation";
 import ArchiveTop from "./ArchiveTop";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-
+import Slider from "react-slick";
 import {
   bath,
   bed,
+  pro_comm1,
+  pro_comm2,
   pro_comm3,
   pro_comm4,
   propert2,
@@ -18,8 +20,8 @@ import {
 } from "../../../assets/images";
 import { CustomPagination } from "../../../Custom_Components/CustomPagination";
 import { pageRoutes } from "../../../router/pageRoutes";
-import ImageWithLoader from "../../../Custom_Components/ImageWithLoader";
-
+import ImageWithLoader from './../../../Custom_Components/ImageWithLoader';
+import   '../../../assets/css/arrow.css'
 const Archive = () => {
   const dispatch = useDispatch();
   const {
@@ -61,7 +63,18 @@ useEffect(() => {
     searchFilters, 
   }));
 }, [dispatch, page, limit, location.search]); 
+// Custom arrow components
+const NextArrow = ({ onClick }) => (
+  <div className="custom-arrow next" onClick={onClick}>
+    <i className="ri-arrow-right-s-line"></i>
+  </div>
+);
 
+const PrevArrow = ({ onClick }) => (
+  <div className="custom-arrow prev" onClick={onClick}>
+    <i className="ri-arrow-left-s-line"></i>
+  </div>
+);
   return (
     <>
       {/* Top Search Bar */}
@@ -247,147 +260,171 @@ useEffect(() => {
           </div>
 
           {/* Dynamic Properties List */}
-          {propertyData &&
-            propertyData?.map((item) => {
-              return (
-                <div className="list_box normal_listing" key={item?._id}>
-                  <div className="row">
-                    <div className="col-lg-5">
-                      <div className="normal_slider">
-                        <div
-                          className="agent_d"
-                          data-bs-toggle="modal"
-                          data-bs-target="#agency_info"
-                        >
-                          <i className="ri-checkbox-circle-fill" /> Checked
-                        </div>
-                        <div className="save_p">
-                          <button>
-                            <i className="ri-heart-line" />
-                          </button>
-                        </div>
-                        <div className="my-slider">
-                          {item?.images &&
-                            item?.images?.map((img, i) => (
-                              <>
-                              <div key={i}>
-                                <Link
-                                  to={
-                                    pageRoutes.PROPERTY_DETAILS +
-                                    `/?id=${item?._id}`
-                                  }
-                                >
-                                  <ImageWithLoader src={img?.url} />
-                                </Link>
-                              </div>
-                              <div key={i}>
-                                <Link
-                                  to={
-                                    pageRoutes.PROPERTY_DETAILS +
-                                    `/?id=${item?._id}`
-                                  }
-                                >
-                                  <ImageWithLoader src={img?.url} />
-                                </Link>
-                              </div>
-                              </>
+       {propertyData &&
+        propertyData.map((item) => {
+          const sliderSettings = {
+            dots: false,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            arrows: true,
+            variableWidth: false,
+            nextArrow: <NextArrow />,
+            prevArrow: <PrevArrow />,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 2,
+                },
+              },
+              {
+                breakpoint: 768,
+                settings: {
+                  slidesToShow: 1,
+                },
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                },
+              },
+            ],
+          };
 
-                            ))}
-                        </div>
-                      </div>
-                      <div className="price_tt normal">
-                        <span>
-                          AED <b>{item?.price}</b> {item?.duration}
-                        </span>
-                        <span className="flex_box">
-                          <img src={user} className="agent_b" />
-                          <i className="ri-verified-badge-fill" />
-                        </span>
-                      </div>
+          return (
+            <div className="list_box normal_listing" key={item?._id}>
+              <div className="row">
+                {/* Left Side */}
+                <div className="col-lg-5">
+                  <div className="normal_slider">
+                    <div
+                      className="agent_d"
+                      data-bs-toggle="modal"
+                      data-bs-target="#agency_info"
+                    >
+                      <i className="ri-checkbox-circle-fill" /> Checked
+                    </div>
+                    <div className="save_p">
+                      <button>
+                        <i className="ri-heart-line" />
+                      </button>
                     </div>
 
-                    <div className="col-lg-7">
-                      <div className="property_data_area">
-                        <h2>
-                          <Link to="/property-details">{item?.title}</Link>
-                        </h2>
-                        <div className="p_info">
-                          <ul>
-                            <li>{item?.subSubCategoryData?.name}</li>
-                            <li>
-                              {item?.bedrooms && (
-                                <span>
-                                  <img src={bed} /> {item?.bedrooms}
-                                </span>
-                              )}
-                              {item?.bathrooms && (
-                                <span>
-                                  <img src={bath} /> {item?.bathrooms}
-                                </span>
-                              )}
-                            </li>
-                            {item?.area && (
-                              <li>
-                                <img src={ruler} /> {item?.area}
-                              </li>
-                            )}
-                          </ul>
-                        </div>
-                        {item?.amenitiesAndFacilitiesData?.length > 0 ? (
-                          <div className="key_property">
-                            <a href="#">
-                              {item?.amenitiesAndFacilitiesData
-                                ?.slice(0, 4)
-                                ?.map((item) => item?.name)
-                                .join(" | ")}
-                            </a>
+                    <div className="my-slider">
+                      <Slider {...sliderSettings}>
+                        {item?.images?.map((img, i) => (
+                          <div key={i}>
+                            <Link
+                              to={`${pageRoutes.PROPERTY_DETAILS}?id=${item?._id}`}
+                            >
+                              <ImageWithLoader src={img?.url} />
+                            </Link>
                           </div>
-                        ) : (
-                          item?.building_facilities?.length > 0 && (
-                            <div className="key_property">
-                              <a href="#">
-                                {item?.building_facilities
-                                  ?.slice(0, 4)
-                                  .join(" | ")}
-                              </a>
-                            </div>
-                          )
+                        ))}
+                      </Slider>
+                    </div>
+                  </div>
+
+                  <div className="price_tt normal">
+                    <span>
+                      AED <b>{item?.price}</b> {item?.duration}
+                    </span>
+                    <span className="flex_box">
+                      <img src={user} className="agent_b" alt="agent" />
+                      <i className="ri-verified-badge-fill" />
+                    </span>
+                  </div>
+                </div>
+
+                {/* Right Side */}
+                <div className="col-lg-7">
+                  <div className="property_data_area">
+                    <h2>
+                      <Link to="/property-details">{item?.title}</Link>
+                    </h2>
+
+                    <div className="p_info">
+                      <ul>
+                        <li>{item?.subSubCategoryData?.name}</li>
+                        <li>
+                          {item?.bedrooms && (
+                            <span>
+                              <img src={bed} alt="bed" /> {item?.bedrooms}
+                            </span>
+                          )}
+                          {item?.bathrooms && (
+                            <span>
+                              <img src={bath} alt="bath" /> {item?.bathrooms}
+                            </span>
+                          )}
+                        </li>
+                        {item?.area && (
+                          <li>
+                            <img src={ruler} alt="area" /> {item?.area}
+                          </li>
                         )}
-                        <div className="pro_desc sli">
-                          {item?.short_description}
-                        </div>
-                        <div className="loc">
-                          <i className="ri-map-pin-line" />{" "}
-                          {item?.locationData?.name}
-                        </div>
-                        <div className="call_action">
-                          <ul>
-                            <li>
-                              <a href="#">
-                                <i className="ri-phone-line" /> Call
-                              </a>
-                            </li>
-                            <li>
-                              <a href="#">
-                                <i className="ri-mail-open-line" /> Email
-                              </a>
-                            </li>
-                            <li>
-                              <a href="#">
-                                <i className="ri-whatsapp-line" /> WhatsApp
-                              </a>
-                            </li>
-                          </ul>
-                          <span>
-                            <img src={property_world_logo} />
-                          </span>
-                        </div>
+                      </ul>
+                    </div>
+
+                    {item?.amenitiesAndFacilitiesData?.length > 0 ? (
+                      <div className="key_property">
+                        <a href="#">
+                          {item?.amenitiesAndFacilitiesData
+                            ?.slice(0, 4)
+                            .map((af) => af?.name)
+                            .join(" | ")}
+                        </a>
                       </div>
+                    ) : (
+                      item?.building_facilities?.length > 0 && (
+                        <div className="key_property">
+                          <a href="#">
+                            {item?.building_facilities
+                              ?.slice(0, 4)
+                              .join(" | ")}
+                          </a>
+                        </div>
+                      )
+                    )}
+
+                    <div className="pro_desc sli">{item?.short_description}</div>
+                    <div className="loc">
+                      <i className="ri-map-pin-line" />{" "}
+                      {item?.locationData?.name}
+                    </div>
+
+                    <div className="call_action">
+                      <ul>
+                        <li>
+                          <a href="#">
+                            <i className="ri-phone-line" /> Call
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">
+                            <i className="ri-mail-open-line" /> Email
+                          </a>
+                        </li>
+                        <li>
+                          <a href="#">
+                            <i className="ri-whatsapp-line" /> WhatsApp
+                          </a>
+                        </li>
+                      </ul>
+                      <span>
+                        <img src={property_world_logo} alt="logo" />
+                      </span>
                     </div>
                   </div>
                 </div>
-              );
-            })}
+              </div>
+            </div>
+          );
+        })}
+
         </div>
 
         {/* Pagination */}

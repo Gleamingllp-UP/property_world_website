9594b73f2267893c236286e3e9963c405b511fa8
rohitsingh7879 <rohitsingh998195@gmail.com>
2025-06-21 11:06:54 +0530

    Added: code for show msg

--- src/pages/auth/signUp/SetPassword.jsx
+++ src/pages/auth/signUp/SetPassword.jsx
@@ -49,7 +49,10 @@ const SetPassword = ({ setStep }) => {
       };
       const resultAction = await dispatch(setPasswordThunk(payload));
       if (setPasswordThunk.fulfilled.match(resultAction)) {
-        showToast("Password successfully set! After Admin Approval You can login.", "success");
+        showToast(
+          "Password successfully set! After Admin Approval You can login.",
+          "success"
+        );
         dispatch(removeUserFormDataToken());
         setTimeout(() => {
           setStep(1);
